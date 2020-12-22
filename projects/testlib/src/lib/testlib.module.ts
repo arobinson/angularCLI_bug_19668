b2acc4dc374cc288c8ed9b4773f07f58fa3b822f
Andrew Robinson <93213+arobinson@users.noreply.github.com>
2020-12-22 11:19:02 -0700

    Add library

---
+++ projects/testlib/src/lib/testlib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { TestlibComponent } from './testlib.component';
+
+
+
+@NgModule({
+  declarations: [TestlibComponent],
+  imports: [
+  ],
+  exports: [TestlibComponent]
+})
+export class TestlibModule { }
